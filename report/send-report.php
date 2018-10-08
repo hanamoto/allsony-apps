28e001b08e7ef6ab1d9b568b792476c0fe2512ca
Masahiro Hanamoto <Masahiro.Hanamoto@sony.com>
2018-10-08 21:13:35 +0900

    fix

--- report/send-report.php
+++ report/send-report.php
@@ -1,12 +1,9 @@
 <?php
 
 // https://github.com/PHPMailer/PHPMailer/tree/5.2-stable
-require './PHPMailer-5.2/PHPMailerAutoload.php';
+require './lib/PHPMailer-5.2/PHPMailerAutoload.php';
 
-define("MAIL_TO",       "allsony.report@gmail.com");
-//define("MAIL_FROM",     "allsony.report@gmail.com");
-define("MAIL_FROM",     "allsony@infostc.org");
-define("MAIL_SUBJECT",  "[AllsonyReport]");
+include 'settings.php';
 
 // 試合結果のイメージを保存するディレクトリ
 define("MATCH_IMG_DIR", "match_img");
@@ -17,13 +14,26 @@ mb_internal_encoding("UTF-8");
 // HTML メールを送信する
 function sendReport($message, $image_path) {
     $mail = new PHPMailer;
-    $mail->addAddress(MAIL_TO); // Add a recipient
-    $mail->setFrom(MAIL_FROM);
-    $mail->Subject = MAIL_SUBJECT;
+
+    // デバッグメッセージを有効にする場合は下記を有効にする
+    //$mail->SMTPDebug = 2;
+
+    // smtp サーバーを明示的に指定してメール送信する
+    $mail->isSMTP();
+    $mail->Host = SMTP_HOST;
+    $mail->Port = SMTP_PORT;
+    $mail->SMTPSecure = 'tls';
+    $mail->SMTPAuth = true;
+    $mail->Username = SMTP_USERNAME;
+    $mail->Password = SMTP_PASSWORD;
+
+    $mail->addAddress(MAIL_TO, "allsony");
+    $mail->setFrom(MAIL_FROM, "allsony");
+    $mail->Subject = "[AllsonyReport]";
     $mail->isHTML(true);
 
     // css は html に直接埋め込む必要がある
-    $css = file_get_contents('report.css');
+    $css = file_get_contents('lib/report.css');
 
     // 画像が送信されていれば、メールにも添付する
     if (!empty($image_path)) {
@@ -45,13 +55,12 @@ $message
 </body>
 </html>
 EOM;
-    $mail->Body = $htmlMessage;
-    /*
+    $mail->msgHTML($htmlMessage);
+
     if(!$mail->send()) {
         echo 'Message could not be sent.';
         echo 'Mailer Error: ' . $mail->ErrorInfo;
     }
-*/
 }
 
 // 送信した内容をブラウザ画面に表示する
@@ -63,7 +72,7 @@ function displayMessage($message, $image_path) {
 <meta charset="utf-8"/>
 <title>Allsony Report</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="report.css">
+<link rel="stylesheet" type="text/css" href="lib/report.css">
 </head>
 <body>
 <h2 color="red">結果を送信しました</h2>
