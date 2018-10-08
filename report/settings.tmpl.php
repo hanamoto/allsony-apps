d4594c4ef80987e611441fc370635cc8e9a04f76
Masahiro Hanamoto <Masahiro.Hanamoto@sony.com>
2018-10-08 21:18:01 +0900

    Fix

---
+++ report/settings.tmpl.php
@@ -0,0 +1,13 @@
+<?php
+
+date_default_timezone_set('Asia/Tokyo');
+
+define("SMTP_HOST",     "smtp.gmail.com");
+define("SMTP_PORT",     587);
+define("SMTP_USERNAME", "XXXX@gmail.com");
+define("SMTP_PASSWORD", "XXXX");
+
+define("MAIL_TO",       "XXXX@gmail.com");
+define("MAIL_FROM",     "XXXX@gmail.com");
+
+?>
